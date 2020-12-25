0e4b298573dff40dd2a03f6a0cc84d3c8eb3865a
Bradley Bath <badfitz66@gmail.com>
2020-12-25 14:51:27 +0000

    Initial commit

---
+++ Assets/Flock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public List<Boids> boids;
+    public Transform goal;
+
+    private void Start()
+    {
+        foreach (Boids boid in boids)
+        {
+            boid.boidsInRange = boids;
+            boid.goal = goal;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+
+    }
+}
